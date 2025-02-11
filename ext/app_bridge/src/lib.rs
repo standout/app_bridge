e4bce20369415b03d64470a3b323300b195d7c61
Alexander Ross <ross@standout.se>
2025-02-11 21:04:25 +0100

    feat: make it work without creating new app

--- ext/app_bridge/src/lib.rs
+++ ext/app_bridge/src/lib.rs
@@ -1,44 +1,17 @@
 use std::cell::RefCell;
 use component::exports::standout::app;
 use magnus::{function, method, prelude::*, Error, Ruby};
-use wasmtime::AsContextMut;
+use wasmtime::{AsContextMut, Store};
 mod component;
+use component::MyState;
+use component::Bridge;
 
-// START GLOBALS
-// use wasmtime::{Engine, Store};
-// use wasmtime::component::Linker;
-// static mut ENGINE: Option<Engine> = None;
-// static mut LINKER: Option<Linker<component::MyState>> = None;
-// static mut STORE: Option<Store<component::MyState>> = None;
-// END GLOBALS
-
-// #[magnus::wrap(class = "AppBridge::App")]
 #[derive(Default)]
 pub struct RApp {
     component_path: String,
-    instance: component::Bridge,
-    store: wasmtime::Store<component::MyState>,
+    instance: RefCell<Option<Bridge>>,
+    store: RefCell<Option<Store<MyState>>>,
 }
-
-// impl RApp {
-//     fn new(component_path: String) -> Self {
-//         // let engine = ENGINE.get_or_insert(component::build_engine());
-//         // let linker = LINKER.get_or_insert(component::build_linker(engine));
-//         // let store = STORE.get_or_insert(component::build_store(engine));
-//         // let (instance, store) = component::load_from_file(component_path).unwrap();
-//         // Self { instance, store }
-//         unsafe {
-//             Self {
-//                 instance: component::app(component_path, ENGINE.unwrap(),STORE.unwrap(), LINKER.unwrap()).unwrap(),
-//                 // store: component::load_from_file(component_path).unwrap().1,
-//             }
-//         }
-//     }
-//     fn rb_get_triggers(&self) -> Vec<String> {
-//         // rb_self.instance.standout_app_triggers().call_get_triggers(rb_self.store).unwrap()
-//         vec!["trigger1".to_string(), "trigger2".to_string()]
-//     }
-// }
 
 #[derive(Default)]
 #[magnus::wrap(class = "AppBridge::App")]
@@ -54,29 +27,21 @@ impl MutRApp {
         let app = component::app(component_path.clone(), engine, &mut store, linker).unwrap();
 
         this.component_path = component_path.to_string();
-        this.instance = app;
-        this.store = store;
+        *this.instance.borrow_mut() = Some(app);
+        *this.store.borrow_mut() = Some(store);
     }
 
     fn get_triggers(&self) -> Vec<String> {
-        // vec!["trigger1".to_string(), "trigger2".to_string()]
-        // let this = self.0.borrow();
-        // let instance = this.instance.as_ref().unwrap();
-        // .call_get_triggers(&this.store).unwrap()
+        let binding = self.0.borrow();
 
-        // let mut this = self.0.borrow_mut();
-        // let instance = &this.instance;
-        // let store = self.0.;
-        // instance.standout_app_triggers().call_get_triggers(store).unwrap()
+        let mut instance = binding.instance.borrow_mut();
+        let mut store = binding.store.borrow_mut();
 
-        let component_path = self.0.borrow().component_path.clone();
-        let engine = component::build_engine();
-        let linker = component::build_linker(&engine).unwrap();
-        let mut store = component::build_store(&engine);
-
-        let app = component::app(component_path, engine, &mut store, linker).unwrap();
-
-        app.standout_app_triggers().call_get_triggers(store).unwrap()
+        if let (Some(instance), Some(store)) = (&mut *instance, &mut *store) {
+            instance.standout_app_triggers().call_get_triggers(store).unwrap()
+        } else {
+            vec![]
+        }
     }
 }
 
