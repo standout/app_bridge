a20f6cdefda977e57d9361c023e805fe9e9de3d3
Alexander Ross <ross@standout.se>
2025-02-14 17:00:23 +0100

    feat: add more object information on inspect

--- lib/app_bridge.rb
+++ lib/app_bridge.rb
@@ -5,5 +5,12 @@ require_relative "app_bridge/app_bridge"
 
 module AppBridge
   class Error < StandardError; end
-  # Your code goes here...
+
+  # Represents a trigger event that is recieved from the app.
+  class TriggerEvent
+    def inspect
+      "#<AppBridge::TriggerEvent(id: #{object_id}, timestamp: #{Time.now}, " \
+        "serialized_data: #{serialized_data.inspect})>"
+    end
+  end
 end
