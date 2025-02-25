88c3bad67a1c4930542016c11381aee5914214a6
Alexander Ross <ross@standout.se>
2025-02-25 16:02:40 +0100

    ci: cross compile

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gemspec
 gem "rake", "~> 13.0"
 
 gem "rake-compiler"
+# gem "rake-compiler-dock"
 
 gem "rspec", "~> 3.0"
 gem "rspec-benchmark"
--- ext/app_bridge/extconf.rb
+++ ext/app_bridge/extconf.rb
@@ -3,4 +3,25 @@
 require "mkmf"
 require "rb_sys/mkmf"
 
+# # Bestäm Rust-target baserat på plattform
+# rust_target = case RbConfig::CONFIG["host_os"]
+#               when /darwin/
+#                 RbConfig::CONFIG["host_cpu"] == "arm64" ? "aarch64-apple-darwin" : "x86_64-apple-darwin"
+#               when /linux/
+#                 case RbConfig::CONFIG["host_cpu"]
+#                 when "x86_64"
+#                   "x86_64-unknown-linux-gnu"
+#                 when "aarch64"
+#                   "aarch64-unknown-linux-gnu"
+#                 else
+#                   raise "Unknown Linux architecture: #{RbConfig::CONFIG["host_cpu"]}"
+#                 end
+#               else
+#                 raise "Unsupported OS: #{RbConfig::CONFIG["host_os"]}"
+#               end
+
+# Sätt rätt Rust target innan byggning
+# ENV["CARGO_BUILD_TARGET"] = rust_target
+
+# Bygg Rust-biblioteket
 create_rust_makefile("app_bridge/app_bridge")
