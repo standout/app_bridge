429162e812ec29f536648d2e16e3308d5f6c6abd
Alexander Ross <ross@standout.se>
2025-04-15 00:11:50 +0200

    chore: bump app_bridge to version 2.0.1

--- spec/fixtures/components/js_app/src/trigger_builder.mjs
+++ spec/fixtures/components/js_app/src/trigger_builder.mjs
@@ -1,4 +1,4 @@
-import { RequestBuilder } from 'standout:app/http@2.0.0';
+import { RequestBuilder } from 'standout:app/http@2.0.1';
 import { AppError } from './app_error.mjs';
 import { safeInt } from './utils.mjs';
 
