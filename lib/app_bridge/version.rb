09b5aac686f6c38d4b5f25aeed0afeae39a06615
Alexander Ross <ross@standout.se>
2025-04-19 23:58:06 +0200

    docs: version update instructions

--- lib/app_bridge/version.rb
+++ lib/app_bridge/version.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# When updating the version, please also update the version in the
+# ext/app_bridge/Cargo.toml file to keep them in sync.
+
 module AppBridge
   VERSION = "2.0.1"
 end
